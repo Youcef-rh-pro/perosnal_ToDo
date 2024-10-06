3ad324cb8828e353c43c0de25e0b55db3a3e42cc
Youcef <youcefriah04@gmail.com>
2024-10-06 23:40:55 +0100

    note are added and stored on local storage and fiexed loading bugs

--- src/index.js
+++ src/index.js
@@ -1,74 +1,86 @@
-
-class note {
+class Note {
   constructor(title, description, importance, duDate) {
     this.title = title;
     this.description = description;
     this.importance = importance;
   }
-render() {
-    const noteEl = document.createElement("li");
-    noteEl.className = "note-item";
-    noteEl.innerHTML = `
-    <div >
-    <h2>${this.title}</h2>
-    <h3>${this.description}</h3>
-    <button>delete</button>
-    </div>
-    `;
-    return noteEl
-}}
-class noteItems {
-  render() {
-    const noteEl = document.createElement("li");
-    noteEl.className = "note-item";
-    noteEl.innerHTML = `
-    <div >
-    <h2>${this.title}</h2>
-    <h3>${this.description}</h3>
-    <button>delete</button>
-    </div>
-    `;
-    return noteEl
 }
-  
+class NoteItems {
+  render(note) {
+    const noteEl = document.createElement("div");
+    // noteEl.className = " card mb-3";
+    noteEl.innerHTML = `
+   <div class="card bg-white m-2  text-dark mb-3">
+  <div class="card-body">
+    <h2 class="card-title">${note.title}</h2>
+    <h3 class="card-text">${note.description}</h3>
+    <button class="btn btn-danger">Delete</button>
+  </div>
+</div>
+    `;
+    return noteEl;
   }
-
-class noteList{
-
-render(notes){
-//  const  getData= JSON.parse(localStorage.getItem('allNotes'))
-const noteGrid =document.getElementById("noteGrid")
-noteGrid.innerHTML=''
-// const noteItem = new noteItems
-  notes.forEach(element => {
-    noteGrid.append(newNote.render(element))
-  })
 }
-}
-class app {
 
-  
-  noteGet() {
-    
-  const  notes = [];const mydata= new noteList 
-   const confirmation= document.getElementById("confirmAddNote")
-   confirmation.addEventListener("click", () => {
-     const noteDescription = document.getElementById("notDescriptionInput").value;
-     const noteName = document.getElementById("noteNameInput").value;
-   
-       
-       notes.push({noteName, noteDescription});
-       localStorage.setItem('allNotes',JSON.stringify(notes) )
-     
-        mydata.render(notes)
-       $('#addNoteModal').modal('hide');
-       document.getElementById('noteNameInput').value = '';
-       document.getElementById('notDescriptionInput').value = '';
-      });
-      
-     
+class NoteList {
+  update() {
+    const noteGrid = document.getElementById("noteGrid");
+    noteGrid.innerHTML = "";
+    this.render(noteGrid);
+  }
+  render(grid) {
+    const notes = JSON.parse(localStorage.getItem("allNotes") || []);
+    if (notes.length === 0) {
+      const noNoteEl = document.createElement("div");
+      // noteEl.className = " card mb-3";
+      noNoteEl.innerHTML = `
+ <div class="card bg-white m-2  text-dark mb-3">
+<div class="card-body">
+  <h2 class="card-title">theres no note</h2>
+  <h3 class="card-text">add note by pressing add Note</h3>
+</div>
+</div>
+  `;
+      grid.append(noNoteEl);
+      return;
     }
+
+    notes.forEach((note) => {
+      const noteItem = new NoteItems();
+      grid.append(noteItem.render(note));
+    });
   }
-  const myApp=new app()
-  myApp.noteGet()
- const newNote = new note
+}
+
+class app {
+  constructor() {
+    this.notes = JSON.parse(localStorage.getItem("allNotes") || []);
+  }
+
+  init() {
+    const noteList = new NoteList();
+    noteList.update();
+    const confirmation = document.getElementById("confirmAddNote");
+    confirmation.addEventListener("click", () => {
+      const noteDescription = document.getElementById(
+        "notDescriptionInput"
+      ).value;
+      const noteName = document.getElementById("noteNameInput").value;
+
+      const newNote = new Note(noteName, noteDescription);
+      this.notes.push(newNote);
+      this.updateLocalStorage();
+
+      $("#addNoteModal").modal("hide");
+      document.getElementById("noteNameInput").value = "";
+      document.getElementById("notDescriptionInput").value = "";
+
+      noteList.update();
+    });
+  }
+  updateLocalStorage() {
+    localStorage.setItem("allNotes", JSON.stringify(this.notes));
+  }
+}
+const myApp = new app();
+myApp.init();
